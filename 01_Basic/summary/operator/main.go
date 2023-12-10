1c9c6e905aa8e15a24e8aab1f1bc69382c7ea1af
lhs960906 <592081451@qq.com>
2023-12-10 19:46:34 +0800

    位运算符补充

--- 01_Basic/summary/operator/main.go
+++ 01_Basic/summary/operator/main.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(uint8(1) & uint8(1)) // 00000001 & 00000001 = 00000001；打印1
-	fmt.Println(uint8(2) & uint8(1)) // 00000010 & 00000001 = 00000000；打印0
+	fmt.Println(uint8(2) << 2) // 00000010 << 2 = 00001000；打印8
+	fmt.Println(uint8(2) >> 2) // 00000010 << 2 = 00000000；打印0
 }
